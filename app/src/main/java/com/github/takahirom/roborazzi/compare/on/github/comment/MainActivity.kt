86e43e60d1056384779ba772e0bab9bf9b1ee70f
Takahiro Menju <takam.dev@gmail.com>
2023-05-04 13:57:41 +0900

    Update MainActivity.kt

--- app/src/main/java/com/github/takahirom/roborazzi/compare/on/github/comment/MainActivity.kt
+++ app/src/main/java/com/github/takahirom/roborazzi/compare/on/github/comment/MainActivity.kt
@@ -39,6 +39,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
   RoborazziscreenshotcompareongithubcommentsampleTheme {
-    Greeting("Android")
+    Greeting("Roborazzi")
   }
 }
