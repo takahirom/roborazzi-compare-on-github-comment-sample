fa848247d99fe697e2adb135b059e99e9eb5c6f9
Takahiro Menju <takam.dev@gmail.com>
2023-05-04 18:03:37 +0900

    Fix to use Roborazzi

--- app/src/main/java/com/github/takahirom/roborazzi/compare/on/github/comment/MainActivity.kt
+++ app/src/main/java/com/github/takahirom/roborazzi/compare/on/github/comment/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
       RoborazziscreenshotcompareongithubcommentsampleTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          Greeting("Roborazzi")
         }
       }
     }
