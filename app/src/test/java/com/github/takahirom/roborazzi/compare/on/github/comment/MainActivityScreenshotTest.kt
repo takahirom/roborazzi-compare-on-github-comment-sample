90319b373c65aca5a29a8a9adc481bc01747f5cd
takahirom <takam.dev@gmail.com>
2023-05-05 22:53:44 +0900

    Add images

--- app/src/test/java/com/github/takahirom/roborazzi/compare/on/github/comment/MainActivityScreenshotTest.kt
+++ app/src/test/java/com/github/takahirom/roborazzi/compare/on/github/comment/MainActivityScreenshotTest.kt
@@ -1,8 +1,11 @@
 package com.github.takahirom.roborazzi.compare.on.github.comment
 
-import androidx.compose.ui.test.isRoot
+import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.takahirom.roborazzi.DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH
 import com.github.takahirom.roborazzi.captureRoboImage
 import org.junit.Rule
 import org.junit.Test
@@ -12,10 +15,19 @@ import org.robolectric.annotation.GraphicsMode
 @RunWith(AndroidJUnit4::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
 class MainActivityScreenshotTest {
-  @get:Rule val composeRule = createAndroidComposeRule<MainActivity>()
+  @get:Rule val composeRule =
+    createAndroidComposeRule<MainActivity>()
+
   @Test
   fun launch() {
-    composeRule.onNode(isRoot())
-      .captureRoboImage()
+    // Espresso
+    onView(ViewMatchers.isRoot())
+      .captureRoboImage(outputPath("espresso_root"))
+
+    // Compose test
+    composeRule.onNode(hasTestTag("hello_text"))
+      .captureRoboImage(outputPath("compose_text"))
   }
+
+  private fun outputPath(name: String) = "$DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH/$name.png"
 }
