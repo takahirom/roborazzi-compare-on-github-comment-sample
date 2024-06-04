f2572ea37ff67a670e8790ca0748324f4768957e
KwakEuiJin <93872496+KwakEuiJin@users.noreply.github.com>
2024-06-05 03:21:12 +0900

    Update MainActivity.kt

--- app/src/main/java/com/github/takahirom/roborazzi/compare/on/github/comment/MainActivity.kt
+++ app/src/main/java/com/github/takahirom/roborazzi/compare/on/github/comment/MainActivity.kt
@@ -21,7 +21,14 @@ class MainActivity : ComponentActivity() {
       RoborazziscreenshotcompareongithubcommentsampleTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          Column {
+            Greeting("Android12312")
+            Greeting("Android12312")
+            Greeting("Android12312")
+            Greeting("Android12312")
+            Greeting("Android12312")
+            Greeting("Android12312")
+          }
         }
       }
     }
